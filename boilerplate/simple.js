571a5d487972a8f2845b4d97074477fdfd905b41
Michael Pearson <github@x0rz.com>
2014-10-07 09:27:09 -0400

    Update simple.js

--- boilerplate/simple.js
+++ boilerplate/simple.js
@@ -3,8 +3,6 @@
  * The Bipio Simple Pod.  boilerplate sample action definition
  * ---------------------------------------------------------------
  *
- * @author Michael Pearson <github@m.bip.io>
- * Copyright (c) 2010-2014 Michael Pearson https://github.com/mjpearson
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
