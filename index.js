d9f38c8affd6c8fbed18f18115bd5eaca86addf3
Alfonso Gober <alfonso@wot.io>
2014-11-24 17:10:09 -0500

    BIP-135 Bridged cdn object to this.

--- index.js
+++ index.js
@@ -224,10 +224,7 @@ Pod.prototype = {
     }
 
     // temporary file management bridge
-    this.$resource.file = {
-      get : this._cdnFileGet,
-      save : this._cdnFileSave
-    }
+    this.$resource.file = cdn;
 
     this.$resource._isVisibleHost = this._isVisibleHost;
 
